5f20004bdfa7e227c61361d10a3887394c60c36c
khankhushi <khushikhan2810@gmail.com>
2023-07-08 01:02:21 +0530

    Creating Server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express"); 
+
+//Creating rest object by whose help we can create the server
+const app = express();
+
+
+// routes
+app.get("/test" ,)
+
+
+//port
+
+const PORT = 8080;
+
+//LISTEN function
+app.listen(PORT, () => {
+    app.listen(PORT, () => {
+        console.log(
+          "Node Server Running"
+        );
+      })
+  })
